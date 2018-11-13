bd7d605ae35e6010f7091a7f5a0d73d098cf3311
Mahesh <mahephpdev@gmail.com>
2018-11-13 15:04:17 +0530

    all

--- strings.php
+++ strings.php
@@ -1,5 +1,25 @@
-<?php 
-$str = "mahesh";
-echo "$str";
+<!--
+String funtions and its built in methods
+strlen;
+strtolower;
+strtoupper;
+ucfirst;
+ucwords;
+strip_tags;
+addslashes;
+stripslashes;
+htmlspecialchars
+html_entity_decode
+strpos
+stripos
+substr
+str_shuffle
+str_shuffle
+chr()
+ord()
+-->
 
+
+<?php 
+echo ord("A");
 ?>
